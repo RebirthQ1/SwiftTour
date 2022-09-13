712f913e6860f16d40f8508c77e4ae453ef35b77
qz619136084 <qz619136084@163.com>
2022-09-13 09:53:43 +0800

    update

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -165,3 +165,24 @@ for os in platforms {
 for i in 1...12 {
     print("5 x \(i) is \(5 * i)")
 }
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
+
+// create an integer to store our roll
+var roll = 0
+
+// carry on looping until we reach 20
+while roll != 20 {
+    // roll a new dice and print what it was
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+// if we're here it means the loop ended – we got a 20!
+print("Critical hit!")
