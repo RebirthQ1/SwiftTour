ce3d7411c89ef2a307e6c5871f8a0b0379425167
qz619136084 <qz619136084@163.com>
2022-11-09 16:18:17 +0800

    update

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -312,3 +312,41 @@ func testFunction(number: Int) -> Int{
     number
 }
 testFunction(number: 1)
+
+//struct Employee {
+//    let name: String
+//    var vacationRemaining: Int
+//
+//    mutating func takeVacation(days: Int) {
+//        if vacationRemaining > days {
+//            vacationRemaining -= days
+//            print("I'm going on vacation!")
+//            print("Days remaining: \(vacationRemaining)")
+//        } else {
+//            print("Oops! There aren't enough days remaining.")
+//        }
+//    }
+//}
+//var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//archer.takeVacation(days: 5)
+//print(archer.vacationRemaining)
+
+struct Employee {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+archer.vacationRemaining = 5
+print(archer.vacationAllocated)
